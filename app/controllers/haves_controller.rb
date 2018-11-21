f7e5331c6f9ccfe4e627ba73cf8a95196b4dbfee
techacademy.challenge <techacademy.challenge@gmail.com>
2018-11-21 09:50:25 +0000

    持っている本追加時のBookモデルとHaveモデルのトランザクションを追加

--- app/controllers/haves_controller.rb
+++ app/controllers/haves_controller.rb
@@ -3,17 +3,18 @@ class HavesController < ApplicationController
     @book = Book.find_or_initialize_by(isbn: params[:book_isbn])
     if @book.new_record?
       @book = Book.new_by_isbn(@book.isbn)
-      if @book.save
+      @book.transaction do
+        @book.save
         current_user.add(@book)
-        flash[:success] = "#{@book.title}を持っているリストに追加しました。"
-      else
-        flash[:danger] = "#{@book.title}を持っているリストに追加できませんでした。"
       end
     else
       current_user.add(@book)
-      flash[:success] = "#{@book.title}を持っているリストに追加しました。"
     end
     
+    flash[:success] = "#{@book.title}を持っているリストに追加しました。"
+    redirect_back(fallback_location: root_path)
+  rescue
+    flash[:danger] = "#{@book.title}を持っているリストに追加できませんでした。"
     redirect_back(fallback_location: root_path)
   end
 
